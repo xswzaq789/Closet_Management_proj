6b219186fdbaae8ddc1657a348039ad8cb2369e2
xswzaq789 <parkhyun0924@naver.com>
2022-06-22 20:16:31 +0900

    AI 06.22 // 20:16

--- Django/apps/image/models.py
+++ Django/apps/image/models.py
@@ -4,10 +4,15 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from config.models import CreationModificationDateBase
 
+# https://proglish.tistory.com/53
+# https://devkor.tistory.com/entry/02-Django-Rest-Framework-%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EC%84%B8%ED%8C%85?category=734691
+
+# https://dev-yakuza.posstree.com/ko/django/models/
+
 
 class ImageModel(CreationModificationDateBase):
     image = models.ImageField(_("image"), upload_to='images')
-
+    url = models.CharField()
     class Meta:
         verbose_name = "Image"
         verbose_name_plural = "Images"
